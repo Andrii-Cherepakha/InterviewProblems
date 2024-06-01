4793fe98aedfd1650e77554862cf540ef377d8e3
Andrii Cherepakha <andrey.cherepakha@gmail.com>
2024-06-02 01:19:51 +0200

    TokenBucket

---
+++ Problems/RateLimiter/TokenBucket.cs
@@ -0,0 +1,51 @@
+﻿namespace Problems.RateLimiter
+{
+    public class TokenBucket
+    {
+        private readonly int capacity;
+        private readonly int refillRate;
+        private readonly int refillPeriod;
+
+        private int size;
+        private DateTime lastUpdated;
+
+        public TokenBucket(int capacity, int refillRate, int refillPeriod)
+        {
+            this.capacity = capacity;
+            this.refillRate = refillRate; // how many
+            this.refillPeriod = refillPeriod; // per how long, seconds
+            size = capacity;
+            lastUpdated = DateTime.UtcNow;
+        }
+
+        public bool IsRequestAllowed(int cost)
+        {
+            if (Size >= cost)
+            {
+                size -= cost;
+                return true;
+            }
+
+            return false;
+        }
+
+        protected internal int Size
+        { 
+            get 
+            {
+                Refill();
+                return size; 
+            } 
+        }
+
+        private void Refill()
+        {
+            DateTime now = DateTime.UtcNow;
+            int secondsLeft = (now - lastUpdated).Seconds; 
+            // int refill = secondsLeft / refillPeriod * refillRate; // precision does matter
+            int refill = (int)(secondsLeft / (double) refillPeriod * refillRate);
+            size = Math.Min(size + refill, capacity);
+            lastUpdated = now;
+        }
+    }
+}
---
+++ Problems/RateLimiter/TokenBucketTest.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+
+namespace Problems.RateLimiter
+{
+    internal class TokenBucketTest
+    {
+        [Test]
+        public void OverCapacity()
+        {
+            int capacity = 10;
+            var tb = new TokenBucket(capacity, 1, 1);
+            Assert.That(tb.Size, Is.EqualTo(capacity));
+            Thread.Sleep(2000);
+            Assert.That(tb.Size, Is.EqualTo(capacity));
+        }
+
+        [Test]
+        public void Refill1TokenPer2Seconds() 
+        {
+            int capacity = 10;
+            var tb = new TokenBucket(capacity, 1, 2);
+            int cost = 6;
+            Assert.That(tb.IsRequestAllowed(cost), Is.True);
+            Assert.That(tb.Size, Is.EqualTo(capacity - cost));
+
+            Thread.Sleep(4200);
+            int expectedRefill = 2;
+            Assert.That(tb.Size, Is.EqualTo(capacity - cost + expectedRefill));
+        }
+
+        [Test]
+        public void Refill10TokensPer2Seconds()
+        {
+            int capacity = 10;
+            var tb = new TokenBucket(capacity, 10, 2);
+            Assert.That(tb.Size, Is.EqualTo(capacity));
+
+            int cost = 8;
+            Assert.That(tb.IsRequestAllowed(cost), Is.True);
+            Assert.That(tb.Size, Is.EqualTo(capacity - cost));
+
+            Thread.Sleep(1100);
+            int expectedRefill = 5;
+            Assert.That(tb.Size, Is.EqualTo(capacity - cost + expectedRefill));
+        }
+
+        [Test]
+        public void NoCapacity()
+        {
+            int capacity = 10;
+            int cost = 6;
+            var tb = new TokenBucket(capacity, 1, 10);
+            Assert.That(tb.IsRequestAllowed(cost), Is.True);
+            Assert.That(tb.IsRequestAllowed(cost), Is.False);
+        }
+    }
+}
